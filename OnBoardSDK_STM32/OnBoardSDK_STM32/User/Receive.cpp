a7bb71b5ec47b9734eda93fe333f30a222d0c249
unknown <yorke.lu@yorkelu.dji.com>
2016-01-07 18:04:57 +0800

    no message

--- OnBoardSDK_STM32/OnBoardSDK_STM32/User/Receive.cpp
+++ OnBoardSDK_STM32/OnBoardSDK_STM32/User/Receive.cpp
@@ -81,9 +81,9 @@ void Rx_buff_Handler()      //when USART recive a 0xfe,start handle command
 						flightData.yaw = Hex2int(Rx_buff[11],Rx_buff[12]);
 						flight.setFlight(&flightData);
 						TIM_Cmd(TIM2,ENABLE);
-						printf("x =%f\n",Hex2int(Rx_buff[5],Rx_buff[6]));
-						printf("y =%f\n",Hex2int(Rx_buff[7],Rx_buff[8]));
-						printf("z =%f\n",Hex2int(Rx_buff[9],Rx_buff[10]));
+						printf("roll_or_x =%f\n",Hex2int(Rx_buff[5],Rx_buff[6]));
+						printf("pitch_or_y =%f\n",Hex2int(Rx_buff[7],Rx_buff[8]));
+						printf("thr_z =%f\n",Hex2int(Rx_buff[9],Rx_buff[10]));
 						printf("yaw =%f\n\n",Hex2int(Rx_buff[11],Rx_buff[12]));
 					}
 					if(Rx_buff[3]==0x00)
@@ -97,10 +97,10 @@ void Rx_buff_Handler()      //when USART recive a 0xfe,start handle command
 					}
 						if(Rx_buff[3]==0x02)
 						{														//for display ur hex2int
-							printf("\nx =%f\n",Hex2int(Rx_buff[5],Rx_buff[6]));
-							printf("y =%f\n",Hex2int(Rx_buff[7],Rx_buff[8]));
-							printf("z =%f\n",Hex2int(Rx_buff[9],Rx_buff[10]));
-							printf("yaw =%f\n",Hex2int(Rx_buff[11],Rx_buff[12]));
+							printf("roll_or_x =%f\n",Hex2int(Rx_buff[5],Rx_buff[6]));
+							printf("pitch_or_y =%f\n",Hex2int(Rx_buff[7],Rx_buff[8]));
+							printf("thr_z =%f\n",Hex2int(Rx_buff[9],Rx_buff[10]));
+							printf("yaw =%f\n\n",Hex2int(Rx_buff[11],Rx_buff[12]));
 							TIM_Cmd(TIM2,ENABLE);
 						}
 				}
